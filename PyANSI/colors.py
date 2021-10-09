3666f2793a2c41d1f264ea066a41c897f02fe6b7
Oliver Jane <>
2021-10-09 11:44:36 +0100

    Updated license

--- PyANSI/colors.py
+++ PyANSI/colors.py
@@ -1,7 +1,3 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at https://mozilla.org/MPL/2.0/.
-
 #print color from hex RGB value
 
 def printHex(*values, sep=" ", end="\n", foreHex = None, backHex = None):
