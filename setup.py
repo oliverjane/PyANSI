8a0a9ffc71054b4c071f96e4902de1ed318e2987
Oliver Jane <22866594+oliverjane@users.noreply.github.com>
2021-11-14 22:01:00 +0000

    Corrected version number in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("./README.md", "r") as fh:
 
 setuptools.setup(
     name="PyANSI",
-    version="1.0.4",
+    version="1.1.0",
     author="Oliver Jane",
     description="Easy ANSI control",
     long_description=long_description,
